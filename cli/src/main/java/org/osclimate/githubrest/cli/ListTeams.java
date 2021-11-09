ec801af52a62b41e4c21c2320ef4d6760c05cce5
Erik Erlandson <eerlands@redhat.com>
2021-11-08 17:15:13 -0700

    UserTeams

--- cli/src/main/java/org/osclimate/githubrest/cli/ListTeams.java
+++ cli/src/main/java/org/osclimate/githubrest/cli/ListTeams.java
@@ -8,22 +8,26 @@ import java.util.List;
 
 class ListTeams {
     public static void main(String[] args) throws Exception {
-	if (args.length != 1) {
-	    System.out.printf("usage: ListTeams <github-org-name>\n");
-	    System.exit(1);
-	}
-	String org = args[0];
-	String apiToken = System.getenv("GITHUB_API_TOKEN");
-        if (apiToken == null) {
-	    System.out.printf("environment variable GITHUB_API_TOKEN must be set\n");
-	    System.exit(1);
-	}
-	System.out.printf("teams for github org %s\n", org);
+        if (args.length != 1) {
+            System.out.printf("usage: ListTeams <github-org-name>\n");
+            System.exit(1);
+        }
+        String org = args[0];
+
+        String apiToken = System.getenv("GITHUB_API_TOKEN");
+            if (apiToken == null) {
+            System.out.printf("environment variable GITHUB_API_TOKEN must be set\n");
+            System.exit(1);
+        }
+
+        System.out.printf("teams for github org %s\n", org);
+
         GitHubRest api = GitHubRetrofit.getClient(GitHubRest.class, "https://api.github.com");
-	retrofit2.Response<List<Team>> response = api.listTeams("Bearer " + apiToken, org, 1000, 1).execute();
+
+        retrofit2.Response<List<Team>> response = api.listTeams("Bearer " + apiToken, org, 1000, 1).execute();
         List<Team> teams = response.body();
-	for (Team t : teams) {
-	    System.out.printf("team: %s\n", t.slug);
-	}
+        for (Team t : teams) {
+            System.out.printf("team: %s\n", t.slug);
+        }
     }
 }
---
+++ cli/src/main/java/org/osclimate/githubrest/cli/UserTeams.java
@@ -0,0 +1,48 @@
+package org.osclimate.githubrest.cli;
+
+import org.osclimate.githubrest.model.Team;
+import org.osclimate.githubrest.model.User;
+import org.osclimate.githubrest.GitHubRetrofit;
+import org.osclimate.githubrest.GitHubRest;
+
+import retrofit2.Response;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+class UserTeams {
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.out.printf("usage: UserTeams <github-org-name> <github-user-name>\n");
+            System.exit(1);
+        }
+        String org = args[0];
+        String user = args[1];
+
+        String apiToken = System.getenv("GITHUB_API_TOKEN");
+        if (apiToken == null) {
+            System.out.printf("environment variable GITHUB_API_TOKEN must be set\n");
+            System.exit(1);
+        }
+
+        GitHubRest api = GitHubRetrofit.getClient(GitHubRest.class, "https://api.github.com");
+
+        Response<List<Team>> response = api.listTeams("Bearer " + apiToken, org, 1000, 1).execute();
+        List<Team> teams = response.body();
+        List<String> userTeams = new ArrayList<>();
+        for (Team t : teams) {
+            Response<List<User>> mresponse = api.listTeamMembers("Bearer " + apiToken, org, t.slug, 1000, 1, "all").execute();
+            List<User> users = mresponse.body();
+            List<String> logins = users.stream().map(u -> u.login).collect(Collectors.toList());
+            if (logins.contains(user)) {
+                userTeams.add(t.slug);
+            }
+        }
+        System.out.printf("teams for user %s in org %s:\n", user, org);
+        for (String t : userTeams) {
+            System.out.printf("    %s\n", t);
+        }
+    }
+}
+
