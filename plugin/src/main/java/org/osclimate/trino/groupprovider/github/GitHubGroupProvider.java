e3b9bd224b92a50f839a1a189e41d7594f935d6e
Erik Erlandson <eerlands@redhat.com>
2021-11-10 15:48:52 -0700

    Fill in biz logic for getGroup

--- plugin/src/main/java/org/osclimate/trino/groupprovider/github/GitHubGroupProvider.java
+++ plugin/src/main/java/org/osclimate/trino/groupprovider/github/GitHubGroupProvider.java
@@ -1,41 +1,68 @@
 package org.osclimate.trino.groupprovider.github;
 
-import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.CacheLoader;
-import com.google.common.cache.LoadingCache;
-import com.google.common.collect.ImmutableMap;
 import io.trino.spi.classloader.ThreadContextClassLoader;
 import io.trino.spi.security.GroupProvider;
 
-import javax.naming.AuthenticationException;
-import javax.naming.NamingEnumeration;
-import javax.naming.NamingException;
-import javax.naming.directory.*;
-import java.util.*;
-import java.util.concurrent.TimeUnit;
-import io.airlift.log.Logger;
-import io.airlift.units.Duration;
+import java.util.List;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.stream.Collectors;
 
-import static javax.naming.Context.*;
+import io.airlift.log.Logger;
+
+import org.osclimate.githubrest.model.Team;
+import org.osclimate.githubrest.model.User;
+import org.osclimate.githubrest.GitHubRetrofit;
+import org.osclimate.githubrest.GitHubRest;
+
+import retrofit2.Response;
+
+import okhttp3.logging.HttpLoggingInterceptor;
 
 public class GitHubGroupProvider implements GroupProvider {
 
-    private static final Logger log = Logger.get(GitHubGroupProvider.class);
+    private final Logger log = Logger.get(GitHubGroupProvider.class);
+    private final String apiURL = "https://api.github.com";
+
+    // from config file:
+    private final String apiToken;
+    private final String githubOrg;
+
+    // set in constructor:
+    private final GitHubRest api;
 
     GitHubGroupProvider(Map<String, String> config) {
-	// load structure parameters from config
-        //this.maxRetryCount = config.getOrDefault("ldap.max-retry-count", "5");
+        this.apiToken = config.get("github.token");
+        this.githubOrg = config.get("github.org");
+
+        this.api = GitHubRetrofit.getClient(
+            GitHubRest.class,
+            apiURL,
+            HttpLoggingInterceptor.Level.BASIC);
     }
 
     @Override
-    public Set<String> getGroups(String user)  {
-	// unsure what the classloader logic is for here
-	try (ThreadContextClassLoader ignored = new ThreadContextClassLoader(getClass().getClassLoader())) {
-            // logic to fill groups from github will go here
-	    Set<String> groups = new HashSet<String>();
-            log.debug("User: [%s], Groups: [%s]", user, groups.toString());
-            return groups;
+    public Set<String> getGroups(String user) {
+        Set<String> groups = new HashSet<String>();
+        // unsure what the classloader logic is for here
+        try (ThreadContextClassLoader ignored = new ThreadContextClassLoader(getClass().getClassLoader())) {
+            Response<List<Team>> response = api.listTeams("Bearer " + this.apiToken, this.githubOrg, 1000, 1).execute();
+            List<Team> teams = response.body();
+            for (Team t : teams) {
+                String team = t.slug;
+                Response<List<User>> mresponse = api.listTeamMembers("Bearer " + this.apiToken, this.githubOrg, team, 1000, 1, "all").execute();
+                List<User> users = mresponse.body();
+                Set<String> logins = users.stream().map(u -> u.login).collect(Collectors.toSet());
+                if (logins.contains(user)) {
+                    groups.add(team.replace('-', '_'));
+                }
+            }
+        } catch (Exception e) {
+            log.error("ERROR in getGroups:\n%s\n", e.toString());
         }
+        log.info("User: [%s], Groups: [%s]", user, groups.toString());
+        return groups;
     }
 }
 
